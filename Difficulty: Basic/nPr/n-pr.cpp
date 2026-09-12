e7de64feeb275dac8efd6f3999fd8e8d82631b83
adityarajput1023 <br88345@gmail.com>
2026-09-12 16:32:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/nPr/n-pr.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    long long nPr(int n, int r) {
+        // code here
+        long long ans=1;
+       for(int i=0;i<r;i++){
+           ans=ans*(n-i);
+       }
+       return ans;
+    }
+};
