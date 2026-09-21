4e03eb04845c1f7c7fe996786ccefc2a9e68941d
Aditya singh Rajput <br88345@gmail.com>
2026-09-21 18:56:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First 1 in a Sorted Binary Array/first-1-in-a-sorted-binary-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int firstIndex(vector<int> &arr) {
+        // code here
+        for(int i=0;i<arr.size();i++){
+            
+            if(arr[i]==1)
+            return i;
+           
+            
+        }
+            return -1;
+        
+    }
+};
