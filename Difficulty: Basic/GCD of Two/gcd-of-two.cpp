efba8f1e4e07cbdb6a098782b1a02df06106658f
Aditya singh Rajput <br88345@gmail.com>
2026-09-24 20:59:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of Two/gcd-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        // code here
+        while(b!=0){
+            int r=a%b;
+            a = b;
+            b = r;
+            
+        }
+        return a;
+    }
+};
